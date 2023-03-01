3bc961ef49a55ae4b464e28d4a4c9c4ec7dd74bf
Chance Jolley <chance.jolley@tlgcohort.com>
2023-03-01 09:25:27 -0800

    JUnit and Assertions

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -9,13 +9,54 @@
 package com.math;
 
 import static org.junit.Assert.*;
-import org.junit.Test;
+
+import org.junit.*;
 
 public class CalculatorTest {
+    private Calculator calc;
 
-  @Test
-  public void testAdd() {
-    Calculator calc = new Calculator();
-    assertEquals(5, calc.add(1, 4));  // expected, actual
-  }
+    @BeforeClass
+    public static void initializeEntireTestRun() {
+        System.out.println("initializeEntireTestRun");
+
+    }
+
+    @Before
+    public void setUp() {
+        System.out.println("setUp");
+        calc = new Calculator();
+
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("tearDown");
+
+    }
+
+    @AfterClass
+    public static void finalizeTestRun() {
+        System.out.println("finalizeTestRun");
+    }
+
+    @Test
+    public void testDivide() {
+        System.out.println("TestDivide");
+        assertEquals(2.5, calc.divide(5,2), 0.001);
+    }
+
+    @Test
+    public void testIsEven() {
+        System.out.println("TestIsEven");
+        assertTrue(calc.isEven(10));
+        assertFalse(calc.isEven(11));
+
+    }
+
+    @Test
+    public void testAdd() {
+        System.out.println("TestAdd");
+        int result = calc.add(1, 4);
+        assertEquals(5, calc.add(1, 4));  // expected, actual
+    }
 }
